5d89f63525175fa1bc07bb1d495e7dab2c19efc1
Thomas M <thomas.ludres@gmail.com>
2021-02-02 00:22:41 +0100

    Initial commit

---
+++ Stars/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Stars
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Point Direction;
+        private List<Star> ListeStars;
+        private Random Rdm;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Initialisation direction
+            Direction = new Point(MainCanvas.Width / 2, MainCanvas.Height / 2);
+
+            //Timer
+            DispatcherTimer Timer = new DispatcherTimer();
+            Timer.Interval = TimeSpan.FromMilliseconds(20);
+            Timer.Tick += Update;
+
+            //Création des étoiles
+            ListeStars = new List<Star>(10);
+            Rdm = new Random();
+            for (int i = 0; i < 500; i++)
+            {
+                Point pos = new Point(Rdm.NextDouble() * MainCanvas.Width, Rdm.NextDouble() * MainCanvas.Height);
+                Star star = new Star(MainCanvas, pos);
+                ListeStars.Add(star);
+            }
+
+            foreach (var star in ListeStars)
+            {
+                Console.WriteLine(star.Position);
+            }
+
+            MainCanvas.Cursor = Cursors.None;
+
+            //Lancer 
+            Timer.Start();
+        }
+
+        public void Update(object sender, EventArgs e)
+        {
+            Direction = Mouse.GetPosition(MainCanvas);
+            List<Star> listeStarsToRemove = new List<Star>();
+            foreach (var star in ListeStars)
+            {
+                star.Update(Direction);
+                if (!star.IsVisible())
+                {
+                    MainCanvas.Children.Remove(star.Shape);
+                    listeStarsToRemove.Add(star);
+                }
+            }
+
+            NewList(listeStarsToRemove);
+        }
+
+        private void NewList(List<Star> list)
+        {
+            foreach (var star in list)
+            {
+                ListeStars.Remove(star);
+                Point pos = new Point(Rdm.NextDouble() * MainCanvas.Width, Rdm.NextDouble() * MainCanvas.Height);
+                ListeStars.Add(new Star(MainCanvas, pos));
+            }
+        }
+    }
+}
---
+++ Stars/Star.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Stars
+{
+    class Star
+    {
+        private Canvas Space;
+        public Point Position;
+        public Line Shape;
+        private Random rdm;
+        private double LongueurInit;
+
+        public Star(Canvas canevas, Point pos)
+        {
+            Space = canevas;
+
+            //Init pos
+            Position = pos;
+
+            //Init shape
+            Shape = new Line();
+            Shape.X1 = 0;
+            Shape.Y1 = 0;
+            SetPosition();
+            rdm = new Random();
+            LongueurInit = rdm.NextDouble() * 5;
+            Shape.Stroke = Brushes.White;
+            Space.Children.Add(Shape);
+        }
+
+        private void SetPosition()
+        {
+            Canvas.SetLeft(this.Shape, Position.X);
+            Canvas.SetTop(this.Shape, Position.Y);
+        }
+
+        private void Move(Vector vect)
+        {
+            Position = Point.Add(Position,vect);
+            Canvas.SetLeft(this.Shape, Position.X);
+            Canvas.SetTop(this.Shape, Position.Y);
+        }
+
+        public bool IsVisible()
+        {
+            bool sortie = !(Position.X < 0);
+
+            if (Position.X > Space.Width)
+            {
+                sortie = false;
+            }
+
+            if (Position.Y < 0)
+            {
+                sortie = false;
+            }
+
+            if (Position.Y > Space.Height)
+            {
+                sortie = false;
+            }
+            return sortie;
+        }
+
+        private void NewPosition(Point center)
+        {
+            Vector direction = Point.Subtract(Position, center);
+            SetDim(direction);
+            direction.Normalize();
+            direction *= 12;
+            Move(direction);
+        }
+
+        private void SetDim(Vector vect)
+        {
+            double distance = vect.Length;
+            vect.Normalize();
+            double dim = LongueurInit + 150 / (Math.Pow(Space.Width, 2) + Math.Pow(Space.Height, 2)) * Math.Pow(distance,2);
+            Point P2 = Point.Add(new Point(0,0), vect * dim);
+            Shape.X2 = P2.X;
+            Shape.Y2 = P2.Y;
+
+            double dim2 = 1 + 3 / (Math.Pow(Space.Width, 2) + Math.Pow(Space.Height, 2)) * Math.Pow(distance, 2);
+            Shape.StrokeThickness = dim2;
+        }
+
+        public void Update(Point direction)
+        {
+            //Calcul de la nouvelle position
+            NewPosition(direction);
+        }
+    }
+}
